a9d282a06918a8aea621dae75e830eb26d31f4bc
YOUR NAME <YOUR@EMAIL.com>
2020-10-20 14:29:00 -0500

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+item1 = { :type => "apple", :color => "red", :price => 1.5 }
+item2 = { :type => "orange", :color => "orange", :price => 1.25 }
+item3 = { :type => "pear", :color => "green", :price => 1.75 }
+puts "The #{item1[:color]} #{item1[:type]} is #{item1[:price]}."
+
+item1 = { type: "apple", color: "red", price: 1.5 }
+item2 = { type: "orange", color: "orange", price: 1.25 }
+item3 = { type: "pear", color: "green", price: 1.75 }
+puts "The #{item1[:color]} #{item1[:type]} is #{item1[:price]}."
