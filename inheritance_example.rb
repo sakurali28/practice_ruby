915270147cf808f575103cbd588d9953b13c34a6
YOUR NAME <YOUR@EMAIL.com>
2020-10-21 14:35:54 -0500

    adds new attributes

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,7 +1,8 @@
-class Vehicle 
+class Vehicle
+  attr_reader :speed, :direction
   def initialize
-    @speed = 0
-    @direction = 'north'
+    @speed = input_options[:speed]
+    @direction = input_options[:direction]
   end
 
   def brake
@@ -18,20 +19,40 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_reader :make, :model
+  def initialize(input_options)
+    @make = input_options [:make]
+    @model = input_options [:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
+  end
+
+  def print_info
+    puts "This is a #{make} #{model}."
   end
 end
 
 class Bike < Vehicle
+  attr_reader :weight, :type
+  def initialize(input_options)
+    @weight = input_options [:weight]
+    @type = input_options[:type]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
+
+  def print_info
+    puts "This #{type} bike weighs #{weight}."
+  end
 end
 
-car = Car.new
-bike = Bike.new
-car.accelerate
-bike.accelerate
+car = Car.new(make: "Honda", model: "Pilot")
+bike = Bike.new(weight: "28lbs", type: "tandem")
 car.honk_horn
 bike.ring_bell
+car.print_info
+bike.print_info
