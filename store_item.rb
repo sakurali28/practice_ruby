5f5be36b4b9d09ed945868cbc03d7d4cd29439e9
YOUR NAME <YOUR@EMAIL.com>
2020-10-21 12:44:32 -0500

    adds input options hash

--- store_item.rb
+++ store_item.rb
@@ -12,21 +12,21 @@ class Store
   attr_reader :type, :color, :price
   attr_writer :price
 
-  def initialize(input_type, input_color, input_price)
-    @type = input_type
-    @color = input_color
-    @price = input_price 
+  def initialize(input_options)
+    @type = input_options[:type]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
   def print_info
-    puts "The #{@color} #{@type} is #{@price}."
+    puts "The #{color} #{type} is #{price}."
   end
 
 end
 
-item1 = Store.new("apple", "red", 1.5)
-item2 = Store.new("orange", "orange", 1.25)
-item3 = Store.new("pear", "green", 1.75)
+item1 = Store.new({type: "apple", color: "red", price: 1.5})
+item2 = Store.new(type: "orange", color: "orange", price: 1.25)
+item3 = Store.new(type: "pear", color: "green", price: 1.75)
 item1.print_info
 item2.print_info
 item3.print_info
